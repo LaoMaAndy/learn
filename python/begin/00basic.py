373bfb4bf7d38b08eae493c09564307bed1a6ca3
andylee765 <andylee765@gmail.com>
2023-05-23 12:01:09 +0800

    Zen of python

---
+++ python/begin/00basic.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+'''
+#import this
+The Zen of Python, by Tim Peters
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+
+Python之禅，作者Tim Peters
+美丽胜于丑陋。
+明了胜于晦涩。
+简洁胜于复杂。
+复杂胜于凌乱。
+扁平胜于嵌套。
+分散胜于密集。
+可读性很重要。
+特例并不足以打破规则。
+虽然实用性胜过纯粹性，但错误不应因此悄悄溜过，除非明确地对此沉默。
+面对模棱两可，拒绝猜测的诱惑。
+应该有一种——最好只有一种——显而易见的方法来做到这一点。
+虽然这种方式可能一开始并不明显，除非你是荷兰人。（这里的 Dutch 是指 Guido）
+现在开始比不做更好。
+然而匆忙开始更糟糕。
+如果方案难以解释，那是个坏方案。
+如果方案容易解释，那可能是个好方案。
+命名空间是一个很棒的想法——让我们做更多的这样的事情！
+
+Index of Python Enhancement Proposals
+    https://peps.python.org/
+PEP 8 – Style Guide for Python Code
+    https://peps.python.org/pep-0008/
+'''
+_ver = ""
+
+if __name__ == "__main__":
+    print("demo:" + __name__)
+#    demo()
