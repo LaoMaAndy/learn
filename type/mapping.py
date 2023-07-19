4cd62c445520d8f3124b9edcbf2459543c8b009f
andylee765 <andylee765@gmail.com>
2023-07-19 17:56:26 +0800

    setup

---
+++ type/mapping.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+r'''
+# Mapping Type 映射类型
+  目前还有一种映射类型 dictionary
+
+# 
+
+
+'''
+
+if __name__ == '__main__':
+    print(__doc__)
+    def test():
+        print('dict.py')
+    test()
