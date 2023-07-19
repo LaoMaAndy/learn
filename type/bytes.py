4218ec338db0efd3d47f9419354ceb696d2cb950
andylee765 <andylee765@gmail.com>
2023-07-19 16:29:44 +0800

    setup

--- type/bytes.py
+++ type/bytes.py
@@ -100,8 +100,89 @@ bytes对象更像整数序列
       未以基于 ASCII 的格式存储的二进制数据可能会导致数据损坏。
 
 # Memory View 内存视图
+访问对象的内部数据，该对象支持 缓冲区协议 buffer protocol 
+
+构建
+    class memoryview(object)
+      创建一个 object 的 memview
+      object必须支持缓冲区协议
+元素
+    指由object处理的内存单元，即
+    每个object的元素可能占一个字节，或多个字节
+
+方法
+    可以使用切片进行读取
+    如果下层对象可写，则支持一维切片赋值
+
+    len(view) 返回元素的数量。特殊的，
+      当 view.ndim = 0时，长度为 1
     
+    __eq__(exporter)
+      如果 tolist() 相等，则相等；
+      如果两边的格式字符串都不被 struct 模块所支持，则两对象比较结果总是不相等
+
+    tobytes(order='C')
+      将缓冲区中的数据作为字节串返回。 
     
+    hex([sep[, bytes_per_sep]])
+      返回一个字符串对象，缓冲区里的每个字节用两个十六进制数码表示
+
+    tolist()
+      将缓冲区内的数据以一个元素列表的形式返回。
+
+    toreadonly()
+      转换为只读
+    
+    release()
+      释放由内存视图对象所公开的底层缓冲区。
+
+    cast(format[, shape])
+      将内存视图转化为新的格式或形状。
+属性
+    itemsize ：每个元素占有的字结数
+    ndim ： 维度
+    readonly ：True / False 是否只读
+    format : 一个字符串，包含视图中每个元素的格式
+    nbytes : 总字节数，只读 nbytes == 
+      product(shape) * itemsize == 
+      len(m.tobytes())
+    obj : 内存视图的下层对象，只读
+    shape : 一个整数元组，
+      通过 ndim 的长度值给出内存所代表的 N 维数组的形状。
+    strides : 一个整数元组，
+      通过 ndim 的长度给出以字节表示的大小，
+      以便访问数组中每个维度上的每个元素。
+    suboffsets
+      供 PIL 风格的数组内部使用。 该值仅作为参考信息。
+    c_contiguous
+      一个表明内存是否为 C-contiguous 的布尔值。
+    f_contiguous
+      一个表明内存是否为 Fortran contiguous 的布尔值。
+    contiguous
+      一个表明内存是否为 contiguous 的布尔值。
+
+# arrary 高效的数值数组
+docs.python.org/zh-cn/3/library/array.html#array.array
+对应于C语言的类型
+
+类型码 C类型    Python类型   以字节表示的最小尺寸 
+'b' signed char     int     1   
+'B' unsigned char   int     1   
+'u' wchar_t Unicode 字符     2 
+'h' signed short    int     2   
+'H' unsigned short  int     2   
+'i' signed int      int     2   
+'I' unsigned int    int     2   
+'l' signed long     int     4   
+'L' unsigned long   int     4   
+'q' signed longlong     int 8   
+'Q' unsigned long long  int 8   
+'f' float           float   4   
+'d' double          float   8   
+
+# struct --- 将字节串解读为打包的二进制数据
+docs.python.org/zh-cn/3/library/struct.html#module-struct
+
 '''
 
 if __name__ == '__main__':
