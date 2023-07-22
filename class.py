19a3d438145eb81a2b77ee5aef56db19db11b681
andylee765 <andylee765@gmail.com>
2023-07-22 11:07:23 +0800

    update

---
+++ class.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+r'''
+# 类
+# 概念：
+    Object Oriented Programming
+    面向对象编程
+    type, instance, attribute, method
+    类型，实例，属性，方法
+    inheritance, base classes, derived class, override
+    继承，基类，派生类，重载
+    created at runtime, modified after creation
+    运行时创建，创建后可以修改
+    member, public, private, virtual
+    成员， 公共， 私有， 虚拟
+    An explicit first argument representing the object
+    方法的第一个参数显式声明对象本身
+    provided implicitly by the call
+    调用方法时隐式提供
+    classes themselves are objects
+    类本身就是对象
+    built-in types can be used as base classes by user
+    用户可以使用内置的作为基类进行扩展
+    built-in operators can be redefined
+    内置运算符可以被重定义
+
+ # 对象和名称：一个对象可以绑定多个名称
+
+ # 作用域和命名空间: Scopes and Namespaces
+ 
+    
+
+
+
+
+'''
+
+if __name__ == '__main__':
+    print(__doc__)
+    def test():
+        print('class.py')
+    test()
