dece1ddf2be5f511bec9ffd9a920c1fb642d114b
andylee765 <andylee765@gmail.com>
2023-07-23 19:55:43 +0800

    update

--- class.py
+++ class.py
@@ -27,6 +27,10 @@ r'''
     命名空间有不同的声明周期
     scope
     作用域
+    Class Object, Instance Object, function object, Method Object
+    类对象、实例对象、函数对象、方法对象
+    Class Variable、Instance Variable
+    类变量、实例变量
 
 # 对象和名称：一个对象可以绑定多个名称
 
@@ -59,16 +63,87 @@ r'''
     非局部变量为只读；如果写入，则会创建一个新的局部变量
     del x 从局部作用域中移除对x的绑定
 
-# 类定义语法
-最简单的定义形式：
+# 类定义语法 - 最简单的定义形式：
     class ClassName:
         pass
     类定义必须先执行才能生效
+    类中的函数定义使用特殊语法
+    当从结尾处正常离开类定义时，将创建一个类对象
+    对象 相当于类定义所创建的命名空间的包装器
+
+# 类对象
+    类对象支持两种操作：属性引用和实例化
+    属性引用: obj.name
+    __doc__ 将返回类的文本字符串
+    类的实例化可看做一个函数调用，返回一个类对象
+    __init__() ：特殊方法，用于创建对象
+
+# 实例对象
+    实例对象的唯一操作是属性引用
+    两种属性名称：数据属性 和 方法
+    数据：无需在类中声明，可直接使用，将自动添加
+      使用dir() 可查看当前属性、方法
+      已经使用的属性，可使用 __dict__ 查看
+    方法：属于对象的函数
+      方法对象 和 函数对象 有区别
+
+# 方法对象
+    x.f() 将立即执行该方法
+    x.f 是一个方法对象，可以被储存，如
+      xf = x.f
+    方法的特殊之处在于，实例对象将作为第一个参数
+      传递给该方法，也就是说：
+      x.f() 与 MyClass.f(x) 完全相同
+
+# 类变量和实例变量
+    类变量为所有的实例共享
+      可以调用类变量的方法，简介修改类变量，此时，所有实例都将同时修改
+      如果将类变量重新赋值，将产生一个局部变量，该变量不影响其他对象的类变量
+    实例变量对于每一个实例不同
+    例如： class foo:
+            name = "Share"   <- 为类变量
+            self.index = 10  <- 为实例变量 
+    共享数据对于涉及可变对象，效果为：
+      若类变量为一个列表，使用列表方法append()后，
+      将同时修改所有实例的类变量
+    
+
 '''
 import sys
-sys.path.append(['./public', '../public']) 
+sys.path.append('./public')
+sys.path.append('../public') 
 from some_func import *
  
+class MyClass():
+    r'''
+    类变量、实例变量
+    '''
+    class_str = 'class_str: not changed'
+    class_list = []
+
+    def __init__(self, s):
+        self.inst_name = s
+        print('Set inst_name: ', s)
+def test_myclass():
+    prn_title('test_myclass')
+
+    a = MyClass('aaa')
+    b = MyClass('bbb')
+    print(a.inst_name)
+    print(b.inst_name)
+    print('Change class_list:')
+    a.class_list.append('Changed by a')
+    print(a.class_list)
+    print(b.class_list)
+    print('Change class_list:')
+    b.class_list.append('Changed by b')
+    print(a.class_list)
+    print(b.class_list)
+    print('Change class_str:')
+    a.class_str = 'class_str changed by a'
+    print(a.class_str)
+    print(b.class_str)
+
 def test_def_class():
     prn_title('test_def_class')
     class MyClass:
@@ -79,4 +154,5 @@ if __name__ == '__main__':
     def test():
         print('class.py')
         test_def_class()
+        test_myclass()
     test()
--- type/with_others.py
+++ type/with_others.py
@@ -63,151 +63,25 @@ r'''
 
 # 其他内置类型
 Modules 模块 
-    模块唯一的特殊操作是属性访问: m.name
-    其中，m 是一个模块， name 访问m中的名称
-    模块的属性 __dict__ 包含了模块的符号表
-    模块符号表中的变量可以被赋值
+    m.name 其中，m 是一个模块， name 访问m中的名称
+    模块的属性 __dict__ 
 
 Classes and Class Instances: 类和类实例
-    参阅：Objects, values and types 对象、值和类型
-         Class definitions 类定义
 
 Functions 函数
-    函数对象是通过函数定义实现的
-    函数的唯一操作是调用它: func(argument-list)
-    实际上存在两种不同的函数对象：内置函数和用户自定义函数。 
-    两者支持同样的操作（调用函数），但实现方式不同，对象类型也不同。
+    唯一操作是调用它: func(argument-list)
 
 Methods 方法
-    方法是使用属性表示法调用的函数。有两种形式：
-      内置方法（例如列表的 append() 方法）和类实例方法。
-    通过实例来访问方法（类命名空间中定义的函数）时：
+    存在两种形式：内置方法（例如列表的 append() 方法）和类实例方法。
+    通过实例来访问方法时：
       你会得到一个特殊对象: 绑定方法 (或称 实例方法) 对象。
       该方法会将 self 参数添加到参数列表。
       绑定方法具有两个特殊的只读属性: 
         m.__self__操作该方法的对象，
         m.__func__ 是实现该方法的函数。
-      与函数对象一样，绑定方法对象支持获取任意属性。但是，
-        由于方法属性实际上存储在底层函数对象 ( meth.__func__ )上，
-        因此不允许在绑定方法上设置方法属性。
-      例如：
-        对于自定义类的对象，可以查看对象方法的__dict__属性：
-          c.method.__func__.__dict__
-        对于自定义函数，可直接查看函数__dict__属性：
-          proc.__dict__
-
-Code Objects 代码对象
-    代码对象指被“伪编译(pseudo-compiled)” 后的函数体
-    不包含对全局执行环境的引用
-    代码对象由内置 compile() 函数返回
-    可以通过 __code__ 属性访问
-    可使用 exec() 和 eval() 执行代码对象，如:
-      eval(f.__code__)
-      
-Type Objects 类型对象
-    代表各种对象类型，对象类型由 type() 访问
-    类型的写法大致为：  <class 'int'>
-    类型没有特殊的操作
-    标准模块 types 定义所有标准内置类型的名称
-
-The Null Object 空对象
-    如果函数没有显式返回值，则默认返回NoneType对象
-    空类型的单实例，命名为None
-    使用 type(None)() 产生同一个单实例
-    不支持任何特殊操作
-
-The Ellipsis Object 省略号对象
-    ... 占位符
-    用法: 
-      查看类型: type(...) 输出 <class 'ellipsis'>
-      函数中: def do_nothing(): ...
-      类型提示: numbers: tuple[int, ...]
-      在 numpy 中, 表示多维列表时， ... 有特殊用法
-    单例：
-      type(Ellipsis)() 会生成 Ellipsis 单实例
-
-The NotImplemented Object 未实现对象
-    单实例。写做： NotImplemented
-    当比较和二元运算被要求对它们不支持的类型进行操作时，会返回该对象。
-    
-Boolean Values 布尔值
-    布尔值是两个常量对象 False 和 True
-    置函数 bool() 可用于将任何值转换为布尔值，
-
-Internal Objects 内部对象
-    参阅 标准类型层级结构。The standard type hierarchy 
-    包含： 栈帧对象、回溯对象以及切片对象等等。
-
-# Special Attributes 特殊属性
-    语言实现为部分对象类型添加了一些特殊的只读属性，
-      它们具有各自的作用。 
-      其中一些并不会被 dir() 内置函数所列出。
-
-object.__dict__
-    一个字典或其他类型的映射对象，用于存储对象的（可写）属性。
-
-instance.__class__
-    类实例所属的类。
-
-class.__bases__
-    由类对象的基类所组成的元组。
-
-definition.__name__
-    类、函数、方法、描述器或生成器实例的名称。
-
-definition.__qualname__
-    类、函数、方法、描述器或生成器实例的 qualified name。
-
-class.__mro__
-    此属性是由类组成的元组，在方法解析期间会基于它来查找基类。
-
-class.mro()
-    此方法可被一个元类来重载，以为其实例定制方法解析顺序。
-      它会在类实例化时被调用，其结果存储于__mro__ 之中。
-
-class.__subclasses__()
-    每个类都存有对直接子类的弱引用列表。
-    本方法返回所有存活引用的列表。
-    列表的顺序按照子类定义的排列。
-
-# 参考： 数据模型 / 标准类型层级结构
+    参考： 数据模型 / 标准类型层级结构
       docs.python.org/zh-cn/3/reference/datamodel.html#types
-
-# 整数字符串转换长度限制
-    Integer string conversion length limitation
-    Exceeds the limit (4300 digits) 
-  使用 sys.set_int_max_str_digits() 设定最大转换位数
-  可配置的最低限制是 640 位:
-    sys.int_info.str_digits_check_threshold
-  需要 import sys
-  
-  受影响的 API
-    该限制仅适用于 int 和 str 或 bytes 之间可能较慢的转换：
-      int(string) 默认基数为 10。
-      int(string, base) 适用于所有不是 2 的幂的底数。
-      str(integer).
-      repr(integer).
-      任何其他字符串转换为基数 10，例如 
-        f"{integer}" 、 
-        "{}".format(integer) 或 
-        b"%d" % integer 
-
-  不受影响的API
-    int(string, base) with base 2, 4, 8, 16, or 32.
-    int.from_bytes() and int.to_bytes().
-    hex(), oct(), bin().
-    格式规格迷你语言 for hex, octal, and binary numbers.
-    str to float.
-    str to decimal.Decimal.
-
-  启动配置 3.11
-    可以使用环境变量或解释器命令行标志来配置限制：
-    PYTHONINTMAXSTRDIGITS, 若 = 0 则无长度限制
-    python3 -X int_max_str_digits=640
-    sys.flags.int_max_str_digits 设定：
-      sys.get_int_max_str_digits() 
-      sys.set_int_max_str_digits()
-    sys.int_info.default_max_str_digits 是编译时的默认限制
+    
 
 '''
 def func_anno(x: int, y: int) -> int:
