9dd62593dc836cb8ec8ed3609d0e898090ec5595
andylee765 <andylee765@gmail.com>
2023-07-27 02:38:21 +0800

    update

---
+++ python/func_buildin.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+r'''
+# 内置函数 Built-in Functions 
+
+
+'''
+
+from share import prn_title
+
+if __name__ == '__main__':
+    print(__doc__)
+    def test():
+        prn_title('func_buildin.py')
+    test()
