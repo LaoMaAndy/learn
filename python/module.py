3a836b20115b74b011a7105e109221942dc81f85
andylee765 <andylee765@gmail.com>
2023-07-26 11:34:30 +0800

    update

--- python/module.py
+++ python/module.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 r'''
+# 模块
+# 关键字 keywords
+    脚本，模块，导入，主模块
+        script, module, imported, main
+
+  模块是包含 Python 定义和语句的文件，其文件名是模块名加后缀名 .py 
+  __name__ 可以获取模块名
+
 '''
 
 from share import prn_title
