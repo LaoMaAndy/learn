6732ba9e87f4a5ad4585ea9f887824dd0840f4c0
andylee765 <andylee765@gmail.com>
2023-11-23 00:45:46 +0800

    exercise

---
+++ html/hello.c
@@ -0,0 +1,166 @@
+/* hello, world */
+#include <stdio.h>
+
+int hello() {
+	int i;
+	printf("Hello, world\n");
+	return 0;
+}
+void swap(int v[], int i, int j) {
+	int temp;
+
+	temp = v[i];
+	v[i] = v[j]
+	v[j] = temp;
+}
+/* hello, world */
+#include <stdio.h>
+
+	int () {
+	int i;
+	printf("Hello, world\n");
+	return 0;
+}
+void swap(int v[], int i, int j) {
+	int temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+/* hello, world */
+/* hello, world */
+/* hello, world */
+/* hello, world */
+#include <stdio.h>
+
+int () {
+	int i;
+	printf("Hello, world\n");
+	return 0;
+}
+void swap(int v[], int i, int j) {
+	int temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+/* hello, world */
+/* hello, world */
+/* hello, world */
+/* hello, world */
+#include <stdio.h>
+
+int () {
+	int i;
+	printf("Hello, world\n");
+	return 0;
+}
+void swap(int v[], int i, int j) {
+	int temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+/* hello, world */
+/* hello, world */
+/* hello, world */
+/* hello, world */
+#include <stdio.h>
+
+int () {
+	int i;
+	printf("Hello, world\n");
+	return 0;
+}
+void swap(int v[], int i, int j) {
+	int temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+/* hello, world */
+/* hello, world */
+/* hello, world */
+/* hello, world */
+#include <stdio.h>
+
+int () {
+	int i;
+	printf("Hello, world\n");
+	return 0;
+}
+void swap(int v[], int i, int j) {
+	int temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+/* hello, world */
+/* hello, world */
+/* hello, world */
+/* hello, world */
+#include <stdio.h>
+
+int () {
+	int i;
+	printf("Hello, world\n");
+	return 0;
+}
+void swap(int v[], int i, int j) {
+	int temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+/* hello, world */
+#include <stdio.h>
+
+int () {
+	int i;
+	printf("Hello, world\n");
+	return 0;
+}
+void swap(int v[], int i, int j) {
+	int temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+/* hello, world */
+#include <stdio.h>
+
+int () {
+	int i;
+	printf("Hello, world\n");
+	return 0;
+}
+void swap(int v[], int i, int j) {
+	int temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+/* hello, world */
+#include <stdio.h>
+
+int () {
+	int i;
+	printf("Hello, world\n");
+	return 0;
+}
+void swap(int v[], int i, int j) {
+	int temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+
