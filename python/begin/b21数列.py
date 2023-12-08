d38d6dafad592fc2d9744b64349868c85815a729
andylee765 <andylee765@gmail.com>
2023-12-08 20:22:12 +0800

    update

--- python/begin/b21数列.py
+++ python/begin/b21数列.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 # 2023-3-15整理
+# 数列
 def fibo(n):
-    count  = 1
+    count  = 0
     a, b = 0, 1
-    while count <= n:
+    print(1, end = ' ')
+    while count < n:
         a, b = b, a + b
-        print(a, end = ' ')
+        print(b, end = ' ')
         count += 1
     return a
+    
 def main():
-    fibo(12)
+    for i in range(10):
+        fibo(i)
+        print()
     print('\n')
 
 print("This is a test")
@@ -22,4 +27,4 @@ for i in range(20):
     print("{:<+10.2f}".format(12.34567890), end ='')
     print("{:#<+10.2f}".format(12.34567890), end ='')
     i += 1
-print()
+    print()
