5038a607d4f28495b6abeabb03c76b3a50a316ce
andylee765 <andylee765@gmail.com>
2023-12-16 13:13:29 +0800

    exercise

---
+++ python/MindMap.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+r'''
+Python3 思维导图
+基础 Basic
+    基本语法 Basic Syntax
+    变量 Variable
+    数据类型 Data Type
+    条件语句 Conditional
+    循环语句 Loop
+    异常 Exception
+    函数 Function
+    复杂类型 List, Tuple, Set, Dictionary
+
+高级 Advanced
+    列表推导 List Comprehension
+    生成器 Generator
+    表达式 Expression
+    闭包 Closure
+    正则表达式 Regex
+    装饰器 Decorator
+    迭代器 Iterator
+    匿名函数 Lamda
+    函数式编程 Functional Programming
+    图，递减，过滤器 Map, Reduce, Filter
+    线程 Thread
+    魔术方法 Magic Method
+
+面向对象编程 OOP
+    类 Class
+    继承 Inheritance
+    方法 Method
+
+包管理 Package Managers
+    pip
+    conda
+
+数据结构和算法 DSA
+    数组和列表 Arrays & Linked lists
+    堆、栈、队列 Heaps, Stacks, Queue
+    哈希表 Hash Tables
+    二分查找 Binary Search
+    树 Trees
+    递归 Recursion
+    排序 Sorting
+    算法 Algorithms
+
+数据科学 Data Science
+    NumPy
+    Pandas
+    Matplotlib
+    Seaborn
+    Scikit-learn
+    TensorFlow
+    Pytorch
+
+Web框架 Web Framework
+    Django
+    Flask
+    FastAPI
+
+测试 Testing
+    单元测试 Unit Testing
+    集成测试 Integration Testing
+    端到端测试 End-to-end testing
+    负载测试 Load Testing
+
+自动化 Automation
+    文件操作 File Manipulations
+    网页修整 Web Scraping
+    图形界面自动化 GUI Automations
+    网络自动化 Network Automation
+
+'''
+
+if __name__ == '__main__':
+    print(__doc__)
